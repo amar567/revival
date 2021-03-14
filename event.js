e6d01e1de90b3cffeed1e87edc9190b1ca4dcaae
Arikta <77336261+Arikta@users.noreply.github.com>
2021-03-14 10:29:43 +0530

    Update event.js

--- event.js
+++ event.js
@@ -580,11 +580,11 @@ function lazarus(){
 
   sub_details.innerHTML=`
   
-  <br>
+  <ul style="list-style-type:disc;">
   <li> <font style="color:red;"> Registrations for Lazarus is closed now!</font> </li>
   <li> <a style="color:blue;" href="./lazarus1.html"> Round-1 Submission Guidelines </a> </li>
   <li> <a style="color:blue;" href="./lazarus2.html"> Round-2 Submission Guidelines </a> </li>
-
+</ul>
   `;
 
   contact.innerHTML=`
