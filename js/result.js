099baee2eaefaf66f00bbc8d0f26720fad497e67
amar567 <60555078+amar567@users.noreply.github.com>
2021-04-02 00:41:47 +0530

    Add files via upload

--- js/result.js
+++ js/result.js
@@ -2,3 +2,8 @@ function laz_results(){
   document.getElementById('container2').style.display='block';
   document.getElementById('container').style.display='none';
 }
+
+function laz2_results(){
+  document.getElementById('container3').style.display='block';
+  document.getElementById('container').style.display='none';
+}
